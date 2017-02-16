7626538c84abe16a1c81696ff1ce0cb777a2a67e
Alexey Voinov <alexey.v.voinov@gmail.com>
2017-02-16 10:05:14 +0100

    fix a warning
    The ex argument in this assetion is not used, and compiler could warn
    you about it.

--- include/cgreen/cpp_assertions.h
+++ include/cgreen/cpp_assertions.h
@@ -8,9 +8,9 @@
         expr;                                                           \
         fail_test("Expected [" STRINGIFY_TOKEN(expr) "] "               \
                 "to throw [" STRINGIFY_TOKEN(exceptionType) "]");       \
-    } catch (const exceptionType& ex) {                                 \
+    } catch (const exceptionType&) {                                    \
         pass_test();                                                    \
-    } catch (const exceptionType* ex) {	                                \
+    } catch (const exceptionType*) {                                    \
         pass_test();                                                    \
     }
 
